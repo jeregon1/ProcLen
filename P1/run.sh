d9d25286eb3377716d9980a5154dd7c82115b782
Jesús López Ansón <839922@unizar.es>
2024-02-28 10:12:32 +0100

    runsh

--- P1/run.sh
+++ P1/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-dir="/mnt/c/Users/jesus/OneDrive/Documentos/3ºCarrera/ProLen/Practicas/prac1"
+dir="/mnt/c/Users/jesus/OneDrive/Documentos/3ºCarrera/ProLen/Practicas/P1"
 
-java -jar "$dir/dist/alike.jar" $1
+# Si hay un argumento, se ejecuta el programa con el argumento, si no, con el fichero ../other/tests/cambio_base.al
+if [ -z "$1" ]
+then
+    java -jar "$dir/dist/alike.jar" "../other/tests/cambio_base.al"
+else
+    java -jar "$dir/dist/alike.jar" "$1"
+fi
