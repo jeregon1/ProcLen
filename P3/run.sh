f4471c53bd08a5d7aa002f6eba3950b0cb46ebac
jeregon <a839922@unizar.es>
2024-04-02 20:23:26 +0200

    verbose mode to print symbol table

--- P3/run.sh
+++ P3/run.sh
@@ -8,19 +8,19 @@ else
 fi
 
 # Comprobar que hay dos parámetros: el fichero alike.jar y el fichero de prueba
-if [ $# -ne 2 ] || [ "$1" != "bobi" ] && [ "$1" != "corni" ]
+if [ $# -lt 2 ] || [ "$1" != "bobi" ] && [ "$1" != "corni" ]
 then
-    echo "Uso: ./run.sh <bobi|corni> <fichero.al>"
+    echo "Uso: ./run.sh <bobi|corni> <fichero.al> [-v]"
     exit 1
 fi
 
 # Si el primer parámetro es "bobi", se ejecuta el compilador $dir/../other/alike/alike.jar
 if [ $1 = "bobi" ]
 then
-    java -jar $dir/../other/alike/alike.jar "$2"
+    java -jar $dir/../other/alike/alike.jar "$2" "$3"
 else
     # Si el primer parámetro es "corni", se ejecuta el compilador $dir/dist/alike.jar
-    java -jar $dir/dist/alike.jar "$2"
+    java -jar $dir/dist/alike.jar "$2" "$3"
 fi
 
 # Su compilador--> java -jar ../other/alike/alike.jar p
