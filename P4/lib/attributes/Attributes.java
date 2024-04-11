a6a9a7c62a0ede85197b5447493a652da8160f90
Javier Sin Pelayo <843442@unizar.es>
2024-04-11 17:27:30 +0200

    escritura en fichero .pcode si no hay errores

--- P4/lib/attributes/Attributes.java
+++ P4/lib/attributes/Attributes.java
@@ -1,11 +1,15 @@
 
 package lib.attributes;
 
+import lib.symbolTable.*;
+import java.util.ArrayList;
 
-public class Attributes {
+public class Attributes implements Cloneable {
     // public Token t;
 
+    public CodeBlock code;
 
-
-    public Attributes() {}
+    public Attributes() {
+        this.code = new CodeBlock();
+    }
 }
