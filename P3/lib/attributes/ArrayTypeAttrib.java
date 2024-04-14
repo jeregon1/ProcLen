5914ff7f51d8d1d0ff3d4b86ccb0ebba91798f61
Javier Sin Pelayo <843442@unizar.es>
2024-04-14 09:19:55 +0200

    fix ArrayTypeAttrib

--- P3/lib/attributes/ArrayTypeAttrib.java
+++ P3/lib/attributes/ArrayTypeAttrib.java
@@ -5,20 +5,22 @@ import lib.symbolTable.*;
 
 // Utilizar en lugar del vector con 1 única componente
 public class ArrayTypeAttrib implements Attributes { 
-	Token tMin, tMax;
-	int min, max, minus1 = 1, minus2 = 1;
-	Symbol.Types baseType;
-	SymbolArray baseArray = null;
-	boolean isParameter = (paramClass != Symbol.ParameterClass.NONE);
-	boolean isDeclaration = !isParameter;
+	private Token tMin, tMax;
+	private int min, max, minus1 = 1, minus2 = 1;
+	private Symbol.Types baseType;
+	private SymbolArray baseArray = null;
 
-    ArrayTypeAttrib() {
+    public ArrayTypeAttrib() {
         minus1 = 1;
         minus2 = 1;
         baseArray = null;
     }
 
-    boolean isParameter() {
-        return param
+    public boolean isParameter() {
+        return paramClass != Symbol.ParameterClass.NONE;
+    }
+
+    public boolean isDeclaration() {
+        return !isParameter();
     }
 }
