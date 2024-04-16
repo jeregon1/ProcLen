4e60ca85b4166805199c4613cd360cedc927d623
Javier Sin Pelayo <843442@unizar.es>
2024-04-16 18:05:33 +0200

    ParVarAttrib :+1:

--- P3/lib/attributes/FuncProcDecAttrib.java
+++ P3/lib/attributes/FuncProcDecAttrib.java
@@ -7,10 +7,16 @@ import java.util.LinkedHashMap;
 
 
 public class FuncProcDecAttrib implements Attributes {
-    public Map <Token,Symbol> params;
+    Token id;
+    Map <Token,Symbol> params;
+    Symbol.Types returnType; // Utilizado solo en caso de función
+    SymbolFunction functionSymbol; // Utilizado solo en caso de función
     // Mirar si se pueden añadir alguno más como "returnType", etc.
 
     public FuncProcDecAttrib() {
+        this.id = null;
         this.params = new LinkedHashMap<Token,Symbol>();
+        this.returnType = Symbol.Types.NONE;
+        this.functionSymbol = null;
     }
 }
--- P3/lib/attributes/ParVarAttrib.java
+++ P3/lib/attributes/ParVarAttrib.java
@@ -4,13 +4,13 @@ package lib.attributes;
 import lib.symbolTable.*;
 
 public class ParVarAttrib implements Attributes {
-    // List<Token> t;
+    List<Token> t;
     SymbolArray paramArray;
     Symbol.Types baseType;
     Symbol.ParameterClass paramClass;
 
     public ParVarAttrib() {
-        // this.t = new ArrayList<Token>();
+        this.t = new ArrayList<Token>();
         this.paramArray = new SymbolArray("");
         this.baseType = Symbol.Types.NONE;
         this.paramClass = Symbol.ParameterClass.VAL;
